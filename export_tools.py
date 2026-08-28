b0e219dcbaa97dd3eefb6f1573d612ce9b949b50
Jun Aishima <jaishima@bnl.gov>
2026-08-28 16:04:02 -0400

    remove old method using Secret
     * the call to this was already removed in the previous
       commit

--- export_tools.py
+++ export_tools.py
@@ -3,14 +3,7 @@ import numpy as np
 from os.path import join
 from autoprocess.statelessAnalysis import get_tes_data, get_tes_rois
 from autoprocess.utils import run_is_processed
-from prefect.blocks.system import Secret
-from tiled.client import from_profile
 import re
-
-
-def initialize_tiled_client(beamline_acronym):
-    api_key = Secret.load(f"tiled-{beamline_acronym}-api-key", _sync=True).get()
-    return from_profile("nsls2", api_key=api_key)[beamline_acronym]["raw"]
 
 
 def get_proposal_path(run):
